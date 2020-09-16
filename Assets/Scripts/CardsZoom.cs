aa17e50ccb10756fb1cc46c6cf47e441e4032116
Devansh Gupta <36420511+devu1999@users.noreply.github.com>
2020-09-16 23:39:43 +0530

    BTP project added

---
+++ Assets/Scripts/CardsZoom.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardsZoom : MonoBehaviour
+{
+    public GameObject Canvas;
+    private GameObject zoomCard;
+
+    public void Awake()
+    {
+        Canvas = GameObject.Find("Main Canvas");
+    }
+
+	public void OnHoverEnter(){
+		zoomCard = Instantiate(gameObject, new Vector2(110, 60), Quaternion.identity);
+		zoomCard.transform.SetParent(Canvas.transform, false);
+		zoomCard.layer = LayerMask.NameToLayer("Zoom");
+		RectTransform  rect = zoomCard.GetComponent<RectTransform>();
+		rect.sizeDelta = new Vector2(80, 120);
+	}
+
+	public void OnHoverExit()
+	{
+		Destroy(zoomCard);
+	}
+}
